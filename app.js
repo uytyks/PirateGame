95b4daa3e2e38b44d2836f285ebd897a80b94297
uytyks <uytyks@udel.edu>
2023-10-12 22:41:31 -0400

    Intro to Upgrade System

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ document.querySelector('#bootyCollector').addEventListener('click', function(){
     console.log(booty);
     document.getElementById("bootyDisplay").innerHTML = "Obtained Booty: " + booty;
 });
+document.getElementById('up1').addEventListener('click', function(){
+    if(booty >= 20){
+        booty -= 20;
+        document.getElementById("bootyDisplay").innerHTML = "Obtained Booty: " + booty;
+        alert("Upgrade bought!");
+    }
+    else{
+        alert("What the fuck man");
+    }
+});
