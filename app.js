5c084615943e200a481dec14d25488a3be8624db
uytyks <uytyks@udel.edu>
2023-10-12 22:05:34 -0400

    Booty Collector

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ let booty = 0;
 document.querySelector('#bootyCollector').addEventListener('click', function(){
     booty++;
     console.log(booty);
+    document.getElementById("bootyCollector").innerHTML = "Active Booty :" + booty;
 });
